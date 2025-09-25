058092902a7e1eb0bebfcaab11582629e897e8e0
sameerkariappa <smuckati@mathworks.com>
2025-09-24 22:52:20 -0700

    Adjusted test case limites for updated Battery contactor fault management protocols

--- Utilities/scripts_CIAutomation/extractTests_toCSV.m
+++
@@ -1,8 +0,0 @@
-function [] = extractTests_toCSV()
-% extractTests_toCSV is used to simulate the BMs, VCU and system level test harnesses
-% and save the result artifacts as CSVs, so others can take advantage of the
-% baseline simulation results as a comparison point for real-world tests.
-
-writeCSV_BMS()
-writeCSV_VCU()
-result = writeCSV_SysModel();
